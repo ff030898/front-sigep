1bb4d84ef11fe8ba09b83549f3ed2641ea1cc97f
ff030898 <fabricioferreiradossantos1998@gmail.com>
2024-07-02 19:04:20 -0300

    Página de Manutenção

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,14 +18,15 @@ export default function Home() {
       <Box flexDirection='row'>
         <Stack spacing={4}>
           <Text fontSize='6xl'>Guiari RH</Text>
-          <Text>Estamos em manutenção e em breve teremos novidades!</Text>
-          <Text>Obrigado pela visita, estamos trabalhando em melhorias.</Text>
-          <Text>Se deseja falar conosco entre em contato com o link abaixo.</Text>
+          <Text fontSize='4xl'>Estamos em manutenção :(</Text>
+          <Text>Em breve teremos novidades!</Text>
+          <Text>Obrigado pela visita, estamos trabalhando em melhorias. <br/> Sempre pensando em você :)</Text>
+          <Text>Se deseja falar conosco entre em contato.</Text>
+          <Text>Clique no botão abaixo!</Text>
           <Button colorScheme='orange' width='250px' p={6} mt={6}>
-            <Link href='https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=atendimento@guiarirh.com.br' isExternal>
+            <Link href='https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=atendimento@guiarirh.com.br' isExternal textDecoration='none'>
               Entre em contato
             </Link>
-
           </Button>
 
         </Stack>
