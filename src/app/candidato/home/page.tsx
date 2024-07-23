46be6d3e5430cac5311be39045b4c3fa566e75b5
hadeares <hadeares@hotmail.com>
2024-07-23 17:41:54 -0300

    ajustes footer header e menuleft

--- src/app/candidato/home/page.tsx
+++ src/app/candidato/home/page.tsx
@@ -1,16 +1,15 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import {
   GridItem,
   Grid
-
 } from '@chakra-ui/react'
 import Header from '@/components/header';
 import MenuLeft from '@/components/menuLeft';
 import Footer from '@/components/footer';
 
-
 export default function Home() {
+  const [isVisible, setIsVisible] = useState(false); 
 
   return (
     <Grid
@@ -23,16 +22,18 @@ export default function Home() {
       gap='0'
     >
       <GridItem pl='2' bg='white' area={'header'} borderBottom='1px solid #ddd'>
-        <Header title='Home'/>
+        <Header title='Home' isVisible={isVisible} />
       </GridItem>
       <GridItem pl='2' bg='teal.800' area={'nav'}>
-        <MenuLeft />
+        <MenuLeft isVisible={false} toggleMenu={function (): void {
+          throw new Error('Function not implemented.');
+        } } />
       </GridItem>
       <GridItem pl='2' bg='gray.50' area={'main'} px={10} py={6}>
         Bem vindo, ao Guiari RH.
       </GridItem>
       <GridItem pl='2' bg='teal.800' area={'footer'}>
-        <Footer />
+        <Footer isVisible={false} />
       </GridItem>
     </Grid>
   );
